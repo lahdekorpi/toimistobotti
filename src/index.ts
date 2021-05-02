42c991d7747dddd7d0dea96268cb5ee4003312ab
Toni Lähdekorpi <toni@Toni-Air.lan>
2021-05-02 19:36:37 +0300

    Add example configs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
 // Conf
 import * as dotenv from "dotenv";
 dotenv.config({ path: __dirname + "/../.env" });
+import * as fs from "fs";
+interface Actions {
+	rfid: { [key: string]: RFID | ButtonRFID };
+}
+
+interface RFID {
+	type: "pir" | "door";
+	name: string;
+}
+
+interface ButtonRFID {
+	name?: string;
+	type: "button";
+	action: string; // Actions are only for the buttons
+}
+
+import YAML from "yaml";
+const actions: Actions = YAML.parse(fs.readFileSync("actions.yaml", "utf8"));
 
 // MQTT
 import * as mqtt from "mqtt";
@@ -42,6 +60,27 @@ client.on("message", (topic, message) => {
 	// Parse JSON if the message contains JSON
 	// Make sure the event is what we are looking for
 	// Make sure if receiving RF data, that the payload is in the known array
+	const rfid = "";
+	const event = actions.rfid[rfid];
+	if (typeof event === "object") {
+		switch (event.type) {
+			case "pir":
+				// Motion detected
+				motionDetected(event.name);
+				break;
+			case "door":
+				doorOpen(event.name);
+				break;
+			case "button":
+				buttonPress(event);
+				break;
+			default:
+				console.warn("Unknown event type", event);
+				break;
+		}
+	} else {
+		console.log("Unknown RFID event");
+	}
 });
 
 console.log("Creating Slack client with token", process.env.SLACK_TOKEN);
@@ -60,8 +99,10 @@ interface ChatPostMessageResult extends WebAPICallResult {
 	// The result is cast to the interface
 	const res = (await web.chat.postMessage({
 		text: "Incoming",
-		username: "toimisto",
-		icon_emoji: "factory",
+		username: "Toimisto",
+		subtype: "bot_message",
+		bot_id: process.env.SLACK_BOT_ID,
+		icon_emoji: "poop",
 		blocks: [
 			{
 				type: "header",
@@ -140,15 +181,77 @@ console.log("Got here for the events");
 
 (async () => {
 	console.log("Creating SlackEvents");
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
-const port = parseInt(process.env.PORT, 10) || 3000;
+	const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
+	const port = parseInt(process.env.PORT, 10) || 3000;
 
-// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-slackEvents.on("message", (event) => {
-  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-});
+	// Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
+	slackEvents.on("message", (event) => {
+		console.log(
+			`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`
+		);
+	});
 
 	console.log("Starting server");
-  const server = await slackEvents.start(port);
-  console.log(`Listening for events on ${server.address()}`);
+	const server = await slackEvents.start(port);
+	console.log(`Listening for events on ${server.address()}`);
 })();
+
+// When a door sensor is triggered
+function doorOpen(name: string = "unknown") {
+	// TODO:
+	// Check if we should alarm
+	if (getAlarm()) {
+		console.log(`Door ${name} opened, alarming`);
+		// Send message to slack
+	} else {
+		console.log(`Door ${name} opened but alarm isn't set, so ignoring`);
+	}
+}
+
+function motionDetected(name: string = "unknown") {
+	// TODO:
+	// Check if we should alarm
+	if (getAlarm()) {
+		console.log(`Motion detected in ${name}, alarming`);
+		// Send message to slack
+	} else {
+		console.log(`Motion detected in ${name} but alarm isn't set, so ignoring`);
+	}
+}
+
+function buttonPress(event: ButtonRFID) {
+	switch (event.action) {
+		case "doorBell":
+			doorBell();
+			break;
+		default:
+			console.warn(`Unknown action ${event.action}`);
+			break;
+	}
+}
+
+// Run if the doorbell has been pressed
+function doorBell() {
+	// TODO:
+	// Doorbell pressed, take an image from the front door camera and send to Slack
+	// Trigger a MQTT event to trigger additional stuff if needed
+}
+
+// Get the current alarmed status
+function getAlarm() {
+	return fs.existsSync(process.env.STATUS_FILE);
+}
+
+// Set the alarmed status
+function setAlarm(status: boolean) {
+	try {
+		if (status) {
+			fs.unlinkSync(process.env.STATUS_FILE);
+		} else {
+			fs.writeFileSync(process.env.STATUS_FILE, new Date().toISOString());
+		}
+	} catch (e) {
+		console.error(e);
+	}
+	return status;
+}
